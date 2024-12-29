1a748ea1dfb97888cbcf62e8672c230dc92415b6
Matheus Melo <melo.matheus13@outlook.com>
2024-12-28 22:13:34 -0300

    Alterações no front (pode talvez dar problema)

---
+++ blossom-crust/src/app/pages/client-login/client-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-login',
+  imports: [],
+  templateUrl: './client-login.component.html',
+  styleUrl: './client-login.component.css'
+})
+export class ClientLoginComponent {
+
+}
---
+++ blossom-crust/src/app/pages/login-selection/login-selection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-selection',
+  imports: [],
+  templateUrl: './login-selection.component.html',
+  styleUrl: './login-selection.component.css'
+})
+export class LoginSelectionComponent {
+
+}
