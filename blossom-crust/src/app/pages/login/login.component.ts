080fb46b999c061b7b5e6fe1bc5395dc5116dfc6
Matheus Melo <melo.matheus13@outlook.com>
2024-12-29 18:52:40 -0300

    Main mais atualizada

--- blossom-crust/src/app/pages/login/login.component.ts
+++ blossom-crust/src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'login-component',
   standalone: true,
-  imports: [RouterModule],
+  imports: [HeaderComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']  // Corrigido para "styleUrls" (plural)
 })
