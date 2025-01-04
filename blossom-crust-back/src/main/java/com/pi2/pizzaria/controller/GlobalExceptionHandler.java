f9fd5730b3c57398a20c49511da1cc9cf0292ac4
GabrielRamiroBR <gabrielramirom@gmail.com>
2025-01-04 19:29:57 -0300

    fazendo as validações funcionarem

---
+++ blossom-crust-back/src/main/java/com/pi2/pizzaria/controller/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.pi2.pizzaria.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        BindingResult bindingResult = ex.getBindingResult();
+
+        for (FieldError error : bindingResult.getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    // Outros manipuladores de exceção podem ser adicionados aqui
+}
